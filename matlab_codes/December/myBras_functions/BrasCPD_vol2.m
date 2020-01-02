7725a64473bdb5b896f931b15f49c0265dc480e4
nina_siam <nina.siaminou@gmail.com>
2020-01-02 17:32:16 +0200

    new terminating conditions and test

--- matlab_codes/December/myBras_functions/BrasCPD_vol2.m
+++ matlab_codes/December/myBras_functions/BrasCPD_vol2.m
@@ -6,6 +6,7 @@ function [A_est, MSE, error] = BrasCPD_vol2(T,options)
     MAX_ITER    = options.max_iter; 
     block_size  = options.bz;
     tol         = options.tol;
+    tol_rel     = options.tol_rel;
     alpha0      = options.alpha0;
     dims        = options.dims;
     %Check if acceleration mode is on 
@@ -23,6 +24,7 @@ function [A_est, MSE, error] = BrasCPD_vol2(T,options)
     iter_per_epoch = floor(dims(1)*dims(2)/block_size(1));                 % epochs
     
     A_est = A_init;
+    A_next = A_init;
     error = error_init;
     MSE = (1/3)*(MSE_measure(A_est{1},A_true{1})+MSE_measure(A_est{2},A_true{2})+ ...
         MSE_measure(A_est{3},A_true{3}));
@@ -37,7 +39,7 @@ function [A_est, MSE, error] = BrasCPD_vol2(T,options)
         n = randi(order,1);                                                % choose factor to update
         kr_idx = find([1:order] - n);                                      % factors for Khatri-Rao 
         J_n = dims(kr_idx(1))*dims(kr_idx(2));                             % khatri rao dimension
-        idx = randperm(J_n,block_size);                                 % choose the sample 
+        idx = randperm(J_n,block_size);                                    % choose the sample 
         F_n = sort(idx);                                                   % sorted sample of row fibers
 
         T_s = T_mat{n};  
@@ -52,22 +54,27 @@ function [A_est, MSE, error] = BrasCPD_vol2(T,options)
             A_next{n} = proxr(A_next{n}, options, n);
 
             Y{n} = A_next{n} + beta_accel*(A_next{n} - A_est{n});
-            A_est{n} = A_next{n};
+            
 
         else
             alpha = alpha0/(block_size*(iter)^(1e-6));        
             A_next{n} = A_est{n} - alpha*(A_est{ n }*H(F_n,:)'*H(F_n,:)-T_s'*H(F_n,:));
             A_next{n} = proxr(A_next{n}, options, n);
-            A_est{n} = A_next{n};
+            
         end
         
-
+        RFC = rel_measure(A_next, A_est);
+        A_est{n} = A_next{n};
+        
+        if RFC < tol_rel 
+            break;
+        end
         if(mod(iter,iter_per_epoch) == 0 && iter > 0)
             i = iter/iter_per_epoch;
             error(i+1) = frob(T - cpdgen(A_est))/frob(T);                      % error for accel scheme
             MSE(i + 1) = (1/3)*(MSE_measure(A_est{1},A_true{1})+MSE_measure(A_est{2},A_true{2})...
                        +MSE_measure(A_est{3},A_true{3}));
-                   
+                  
             if abs(error(i+1) )<= tol
                  break;
             end
