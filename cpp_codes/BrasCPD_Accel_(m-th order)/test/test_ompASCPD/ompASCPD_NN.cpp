4681ca997ac6370e458df22a5612e9f5974b7047
Nina Siaminou <nina.siaminou@gmail.com>
2020-11-12 15:00:27 +0200

    initial commit test parallel

---
+++ cpp_codes/BrasCPD_Accel_(m-th order)/test/test_ompASCPD/ompASCPD_NN.cpp
@@ -0,0 +1,105 @@
+#define EIGEN_DONT_PARALLELIZE 
+#define USE_COST_FUN 0
+#define WRITE_DATA 0
+
+#include "master_library.hpp"
+
+
+#define INITIALIZED_SEED 1                                        // if initialized seed is on the data will be different for every run (including the random fibers)
+                                                                  // EDIT: now only the fibers will change due to seeder, which is initialized after init and true factors are created
+void Write_to_File(int nrows, int ncols, Ref<MatrixXd> Mat, const char *file_name){
+	ofstream my_file(file_name, ios::out | ios::binary | ios::trunc);
+	if (my_file.is_open()){
+		my_file.write((char *) Mat.data(), nrows*ncols*sizeof(double));
+		my_file.close();
+	}
+	else
+		cout << "Unable to open file \n";
+}
+
+int main(int argc, char **argv){
+
+    const int TNS_ORDER = 3;                                      // Declarations
+    const int R = 10;
+    const int p = 100;
+
+    VectorXi tns_dims(TNS_ORDER);
+    VectorXi block_size(TNS_ORDER);
+
+    Eigen::Tensor< double, 0 > f_value;
+    Eigen::Tensor< double, TNS_ORDER > True_Tensor;
+    Eigen::Tensor< double, TNS_ORDER > Init_Tensor;
+    std::array<MatrixXd, TNS_ORDER> Init_Factors;
+    std::array<MatrixXd, TNS_ORDER> True_Factors;
+
+    // Assign values
+    tns_dims.setConstant(500); 
+    block_size.setConstant(200);
+
+    //Initialize the tensor
+    for(size_t factor = 0; factor < TNS_ORDER; factor++ )
+    {
+        True_Factors[factor] = (MatrixXd::Random(tns_dims(factor), R) + MatrixXd::Ones(tns_dims(factor), R))/2;
+        #if WRITE_DATA
+            std::string file_name = "../Data_cpp/true_factor_";
+            std::string file_subfix = std::to_string(factor);
+            std::string file_extension = ".bin";
+            std::string file_name1 = file_name + file_subfix + file_extension;
+
+            MatrixXd Factor_to_write = True_Factors[factor].transpose();
+            Write_to_File(tns_dims(factor), R, Factor_to_write, file_name.c_str());
+        #endif
+    }
+   
+    nanoseconds stop_t_tns_cr = 0s;
+    auto start_tns_cr = high_resolution_clock::now();
+    // Create True Tensor
+    CpdGen( tns_dims, True_Factors, R, True_Tensor);
+
+    auto end_tns_cr = high_resolution_clock::now();
+    stop_t_tns_cr = duration_cast<std::chrono::nanoseconds>(end_tns_cr - start_tns_cr);
+
+
+    cout << "Tensor of order: " << TNS_ORDER << "\t ~Dimensions: " << tns_dims.transpose() << "\t ~Rank: "<< R << endl;
+    cout << "Time Create True Tensor (CPDGEN): " << stop_t_tns_cr.count() * (1e-9) << "s" << endl;  
+    cout << "Sampling of each mode with blocksize: " << block_size.transpose() << endl;
+
+    // Frobenius norm of Tensor
+    Eigen::Tensor< double, 0 > frob_X  = True_Tensor.square().sum();  
+    cout << "Frob_X:"  << frob_X << endl; 
+
+    double* Tensor_pointer = True_Tensor.data();
+
+
+    // Create Init Factors, Write factors to Matlab to compare the results
+    for(size_t factor = 0; factor < TNS_ORDER; factor++ )
+    {
+        Init_Factors[factor] = (MatrixXd::Random(tns_dims(factor), R) + MatrixXd::Ones(tns_dims(factor), R))/2;
+        #if WRITE_DATA
+            std::string file_name1 = "../Data_cpp/initial_factor_";
+            std::string file_subfix1 = std::to_string(factor);
+            std::string file_extension1 = ".bin";
+            file_name1 = file_name1 + file_subfix1 + file_extension1;
+
+            MatrixXd Factor_to_write = Init_Factors[factor].transpose();
+            Write_to_File(tns_dims(factor), R, Factor_to_write, file_name1.c_str());
+        #endif
+    }
+    CpdGen( tns_dims, Init_Factors, R, Init_Tensor);
+    f_value = (True_Tensor - Init_Tensor).square().sum(); 
+
+    VectorXi zero_vector(TNS_ORDER);
+    zero_vector.setZero();
+    Init_Tensor.resize(zero_vector);
+
+    double AO_tol = 0.001;
+    int MAX_MTTKRP = 40;
+    
+    #if INITIALIZED_SEED                                          // Initialize the seed for different data
+        srand((unsigned) time(NULL));                             // n case different before true and init factors
+    #endif
+
+    
+    parallel::solve_BrasCPaccel(AO_tol, MAX_MTTKRP, R, frob_X, f_value, tns_dims, block_size, Init_Factors, Tensor_pointer, True_Tensor, p);
+    return 0;
+}
