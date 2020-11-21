95d6b8efc4d0a9748f133f59de7c9072aa139931
Nina Siaminou <nina.siaminou@gmail.com>
2020-11-21 16:42:33 +0200

    feat: test sorted KR for 3-mode tensor too

--- cpp_codes/BrasCPD_Accel_(m-th order)/test/test_sampling_sorted/sampling_fibers_sorted.cpp
+++ cpp_codes/BrasCPD_Accel_(m-th order)/test/test_sampling_sorted/sampling_fibers_sorted.cpp
@@ -17,8 +17,8 @@ int main(int argc, char **argv){
     std::array<MatrixXd, TNS_ORDER> True_Factors;
 
     // Assign values
-    tns_dims.setConstant(20); 
-    block_size.setConstant(20);
+    tns_dims.setConstant(3); 
+    block_size.setConstant(3);
     ////////////////////////////////////////////////////////////////////////////////////////
 
     // Initialize the tensor using tensor module
@@ -41,18 +41,36 @@ int main(int argc, char **argv){
     cout << "Sampling of each mode with blocksize: " << block_size.transpose() << endl;
     cout << "True Tensor: " << "\n" << True_Tensor << endl;
 
-    int mode = 0;
+    int mode = 1;
     MatrixXi idxs(block_size(mode),TNS_ORDER);
-    MatrixXi factor_idxs(block_size(mode),TNS_ORDER-1);
     MatrixXd T_mode(tns_dims(mode), block_size(mode));
-    
-    cout << "Test 0: " << endl; 
-    sorted::Sample_fibers<TNS_ORDER>(Tensor_pointer,  tns_dims,  block_size,  mode,
-                      idxs, T_mode);
+    MatrixXd KR_sampled(tns_dims(1)*tns_dims(2), R);
+    MatrixXd KR_Full(tns_dims(1)*tns_dims(2), R);
 
-    cout << "idxs: " << "\n" << idxs << endl;
-    cout << "factor_idxs: " << "\n" << factor_idxs << endl;
-    cout << "Matricization mode 0 = \n " << T_mode << endl;
+    cout << "Test 0: " << endl; 
+    std::vector<std::vector<int>> fibers_idxs =  sorted::Sample_fibers<TNS_ORDER>(Tensor_pointer, tns_dims, block_size, mode,
+                    idxs, T_mode);
+    // Number of rows; 
+    int m = fibers_idxs.size();  
+    int n = fibers_idxs[0].size(); 
+
+    cout << "idxs (Full idxs list before sorting): " << "\n" << idxs << endl;
+    // Displaying the 2D vector after sorting 
+    cout << "The Matrix after sorting 1st row is:\n"; 
+    for (int i=0; i<m; i++) 
+    { 
+        for (int j=0; j<n ;j++) 
+            cout << fibers_idxs[i][j] << " "; 
+        cout << endl; 
+    } 
+    cout << "Matricization mode ="<< mode << " \n " << T_mode << endl;
+
+    // sampling KR-product
+    sorted::Sample_KhatriRao(mode, R, fibers_idxs, True_Factors, KR_sampled);
+    cout << "KR sampled = \n " << KR_sampled << endl;
+
+    Khatri_Rao_Product(True_Factors[2], True_Factors[0], KR_Full);
+    cout << "KR FULL = \n " << KR_Full << endl;
 
     return 0;
 
